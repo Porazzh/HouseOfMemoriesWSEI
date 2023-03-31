054038fbf00e6215b80e2946bc2377eac0b46d62
giga szew B) <63122065+Porazzh@users.noreply.github.com>
2023-03-31 18:00:33 +0200

    Menu1#

---
+++ Assets/Scripts/sceneLoader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneLoader : MonoBehaviour
+{
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
