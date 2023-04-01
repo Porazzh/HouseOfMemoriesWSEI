bd5a940ba4c047e3cb4fbc14b1ad2688cf78d3a3
Entity12335 <113011476+Entity12335@users.noreply.github.com>
2023-04-01 04:38:17 +0200

    tak

---
+++ Assets/Scripts/zegar/zegar.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class zegar : MonoBehaviour
+{
+    public Camera cameramain; 
+    private Vector3 mouse_pos;
+    public Transform target;
+    private Vector3 object_pos;
+    private float angle;
+    
+
+    
+
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            mouse_pos = Input.mousePosition;
+            mouse_pos.z = -20;
+            object_pos = cameramain.WorldToScreenPoint(target.position);
+            mouse_pos.x = mouse_pos.x - object_pos.x;
+            mouse_pos.y = mouse_pos.y - object_pos.y;
+            angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+        }
+       
+    }
+}
